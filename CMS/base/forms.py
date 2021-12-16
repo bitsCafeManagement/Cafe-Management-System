4647ca49b8078fde5744e5dd998e17a493773540
nigussolomon <nigussave@gmail.com>
2021-12-16 10:43:40 +0300

    Upgraded the theme

--- CMS/base/forms.py
+++ CMS/base/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import models
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.forms import fields
@@ -6,15 +7,13 @@ from django.forms import fields
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=60)
-    full_name = forms.CharField(max_length=60)
     
     class Meta:
         model = User
-        fields = ['full_name','username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
         
 class AuthenticateForm(AuthenticationForm):
     class Meta:
         username = forms.CharField(max_length=60, label= 'Email/Username')
         model = User
         fields = ['username', 'password']
-    
